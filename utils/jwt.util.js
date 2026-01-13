8a66038257f974d9269ecd6786e793c1704e7a71
Amir Suhel <amirsuhel5003@gmail.com>
2026-01-13 21:34:55 +0530

    cookies issue fix

--- utils/jwt.util.js
+++ utils/jwt.util.js
@@ -33,19 +33,24 @@ const verifyRefreshToken = (token) => {
  * Set JWT tokens in HTTP-only cookies
  */
 const setTokenCookies = (res, accessToken, refreshToken) => {
+  const isProduction = process.env.NODE_ENV === 'production';
+  
+  const cookieOptions = {
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? 'none' : 'lax', // 'none' for cross-origin in production
+    path: '/',
+  };
+
   // Access token cookie (15 minutes)
   res.cookie('accessToken', accessToken, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    ...cookieOptions,
     maxAge: 15 * 60 * 1000 // 15 minutes
   });
 
   // Refresh token cookie (7 days)
   res.cookie('refreshToken', refreshToken, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    ...cookieOptions,
     maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
   });
 };
@@ -54,15 +59,18 @@ const setTokenCookies = (res, accessToken, refreshToken) => {
  * Clear JWT cookies
  */
 const clearTokenCookies = (res) => {
-  res.cookie('accessToken', '', {
-    httpOnly: true,
-    expires: new Date(0)
-  });
+  const isProduction = process.env.NODE_ENV === 'production';
   
-  res.cookie('refreshToken', '', {
+  const cookieOptions = {
     httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? 'none' : 'lax',
+    path: '/',
     expires: new Date(0)
-  });
+  };
+  
+  res.cookie('accessToken', '', cookieOptions);
+  res.cookie('refreshToken', '', cookieOptions);
 };
 
 module.exports = {
